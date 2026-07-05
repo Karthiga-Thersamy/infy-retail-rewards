d18d9235adbe9cac883ad520fc3be9294df120a6
unknown <karthigathenraj@gmail.com>
2026-07-05 21:03:55 +0530

    Retail Rewards Application - missed files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO CUSTOMER (id, name) VALUES (1, 'Karthiga');
+INSERT INTO CUSTOMER (id, name) VALUES (2, 'Thersamy');
+
+INSERT INTO TRANSACTION (customer_id, amount, date) VALUES (1, 120, '2026-04-10');
+INSERT INTO TRANSACTION (customer_id, amount, date) VALUES (1, 75, '2026-05-15');
+INSERT INTO TRANSACTION (customer_id, amount, date) VALUES (2, 200, '2026-06-20');
